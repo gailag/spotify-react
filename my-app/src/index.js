dadff16e479b727e640d6a6a6366d3013e43be2c
Gaila Galano <gailag@hawaii.edu>
2022-03-03 17:04:59 -1000

    updated progress

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Image, Menu } from 'semantic-ui-react';
 
 //top menu
 class TopMenu extends React.Component {
   render() {
     return (
-        <Header as="h1">TopMenu</Header>
+        <Container style={{padding: 20}}>
+          <Menu borderless className="topmenu">
+            <Image size="small" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" />
+            <Menu.Item position="right">Premium</Menu.Item>
+            <Menu.Item>Support</Menu.Item>
+            <Menu.Item>Download</Menu.Item>
+            <Menu.Item>|</Menu.Item>
+            <Menu.Item>Sign up</Menu.Item>
+            <Menu.Item>Log in</Menu.Item>
+          </Menu>
+        </Container>
     );
   }
 }
